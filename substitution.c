9c695f6fd628cdff1432e085bffe9d997e20f8ec
Chase Taylor <chase@Chases-MacBook-Pro.local>
2021-04-26 14:55:46 -0700

    Finished substitution project. 100% via check50

--- substitution.c
+++ substitution.c
@@ -12,8 +12,10 @@ int main(int argc, string argv[])
         printf("Usage: ./substitution KEY\n");
         return 1;
     }
+
     //Checking for argument input compatibility
-    else if(argc == 2)
+
+    else if (argc == 2)
     {
         for (int array_counter = 0, argument = strlen(argv[1]); array_counter < argument; array_counter++)
         {
@@ -28,28 +30,73 @@ int main(int argc, string argv[])
                 return 1;
             }
         }
-        //Testing how to find duplicate letters BUT ITS NOT WORKING... maybe write a formula converting letters to upper case
-        //adding all together A through Z and if the number is not == to user input it means a letter was duplicated
+
+        //Checking for duplicate letters
 
         string argument_input = argv[1];
 
-        printf("%s\n", argument_input);
+        //Create placeholder for a copy of user input
+
+        char copied_input[27];
+
+        //Making a copy of user input in order to manipulate to test for duplicate letters
+
+        strcpy(copied_input, argument_input);
+
+        //Make copied input all caps to check for duplicate letters
+
+        for (int i = 0; i < strlen(copied_input); i++)
+        {
+            if (islower(copied_input[i]))
+            {
+                copied_input[i] -= 32;
+            }
+        }
+
         int duplicate_counter = 0;
 
-        printf("Duplicate characters: \n");
-
-        for (int i = 0; i < strlen(argument_input); i++)
+        for (int i = 0; i < strlen(copied_input); i++)
         {
 
-            for (int j = i + 1; j < strlen(argument_input); j++)
+            for (int j = i + 1; j < strlen(copied_input); j++)
             {
-                if(argument_input[i] == argument_input[j])
+                if (copied_input[i] == copied_input[j])
                 {
                     duplicate_counter++;
-                    printf("%c\n",argument_input[i]);
+                    printf("Key must not contain repeated characters\n");
+                    return 1;
                 }
             }
         }
-        printf("%i\n",duplicate_counter);
     }
+
+    //Prompt user for plaintext
+
+    string plaintext = get_string("plaintext: ");
+
+    printf("ciphertext: ");
+
+    //Convert plaintext into ciphertext and print
+
+    for (int array_plaintext_counter = 0, input_plaintext = strlen(plaintext); array_plaintext_counter < input_plaintext;
+         array_plaintext_counter++)
+    {
+        if (isupper(plaintext[array_plaintext_counter]))
+        {
+            plaintext[array_plaintext_counter] -= 65;
+            int key = plaintext[array_plaintext_counter];
+            printf("%c", toupper(argv[1][key]));
+        }
+        else if (islower(plaintext[array_plaintext_counter]))
+        {
+            plaintext[array_plaintext_counter] -= 97;
+            int key = plaintext[array_plaintext_counter];
+            printf("%c", tolower(argv[1][key]));
+        }
+        else
+        {
+            printf("%c", plaintext[array_plaintext_counter]);
+        }
+    }
+    printf("\n");
 }
